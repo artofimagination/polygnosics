293b2034c054f1cf860373f3b708bc924991e028
Peter Devenyi <potidannon@gmail.com>
2020-11-05 09:07:43 +1300

    Issue#22: Removed omit empty from user struct

--- app/models/user.go
+++ app/models/user.go
@@ -7,8 +7,8 @@ import (
 // User defines the user structures. Each user must have an associated settings entry.
 type User struct {
 	ID         uuid.UUID `json:"id" validation:"required"`
-	Name       string    `json:"name,omitempty" validation:"required"`
-	Email      string    `json:"email,omitempty" validation:"required"`
-	Password   string    `json:"password,omitempty" validation:"required"`
+	Name       string    `json:"name" validation:"required"`
+	Email      string    `json:"email" validation:"required"`
+	Password   string    `json:"password" validation:"required"`
 	SettingsID uuid.UUID `json:"user_settings_id" validation:"required"`
 }
